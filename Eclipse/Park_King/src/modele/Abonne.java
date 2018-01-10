52bff8c9abbcfdad6e11a6e5ec8a67eddadb0ebc
DarkVasDehors <sibade.arnaud@gmail.com>
2018-01-10 11:55:02 +0100

    KODE : debut des modeles

---
+++ Eclipse/Park_King/src/modele/Abonne.java
@@ -0,0 +1,22 @@
+package modele;
+
+public class Abonne extends Client{
+	private int numAbonne;
+	
+	public Abonne() {
+		this.numAbonne = 666;
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getNumAbonne() {
+		return numAbonne;
+	}
+
+	public void setNumAbonne(int numAbonne) {
+		this.numAbonne = numAbonne;
+	}
+	
+	public void makeEntretiens(){
+		//TODO
+	}
+}
