e479cda1f1fe8854591d8b8535b967ab5fbae08b
DarkVasDehors <sibade.arnaud@gmail.com>
2018-01-11 16:50:41 +0100

    VIEW : javaFX main

---
+++ Eclipse/Park_King/src/vue/UserViewMain.java
@@ -0,0 +1,16 @@
+package vue;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UserViewMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
