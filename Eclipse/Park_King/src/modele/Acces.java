aa66083f368c4c7101f409bfef07d18302bbd152
YoannGa <yoann.gath@gmail.com>
2018-01-16 14:22:11 +0100

    Acces.java

---
+++ Eclipse/Park_King/src/modele/Acces.java
@@ -0,0 +1,16 @@
+package modele;
+
+public class Acces {
+
+	private Camera cam;
+	private Borne borne;
+	
+	public boolean checkPresenceVehicule() {
+		return cam.vehiculePresent();	
+		
+	}
+	
+	
+	
+	
+}
